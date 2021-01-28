54c7b4fde9d5691a8b0c677ebc4c5515bfcd109c
yinxiaodi <809073214@qq.com>
2021-01-28 18:59:12 +0800

    登录页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,8 @@
 $(function() {
 
     //提取layui中的form表单模块
-    let form = layui.form;
-    // const { form } = layui
+    // let form = layui.form;
+    const { form, layer } = layui
 
 
     //点击切换登录和注册表单
@@ -24,9 +24,33 @@ $(function() {
     //实现注册功能
     $('.reg-form').submit(function(e) {
         e.preventDefault();
-        axios.post('http://ajax.frontend.itheima.net/api/reguser', $(this).serialize())
+        console.log($(this).serialize());
+        //发送ajax
+        axios.post('/api/reguser', $(this).serialize())
             .then(res => {
-                console.log(res.data);
+                if (res.status !== 0) {
+                    return layer.msg('注册失败!')
+                }
+                layer.msg('注册成功!')
+                $('.login-form a').click()
             })
-    })
+    });
+    //实现登录功能
+    $('.login-form').submit(function(e) {
+        e.preventDefault();
+        console.log($(this).serialize());
+        //发送ajax请求
+        axios.post('/api/login', $(this).serialize())
+            .then(res => {
+                console.log(res);
+                //验证是否登录成功
+                if (res.status !== 0) {
+                    return layer.msg('登录失败!')
+                }
+                //登录成功后将res.token 个人身份凭证保存在本地
+                localStorage.setItem('token', res.token);
+                //跳转到首页
+                location.href = './index.html'
+            })
+    });
 })
