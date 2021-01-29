cc4a9e39937e9383293b5ba9223f9e6f958df1c5
yinxiaodi <809073214@qq.com>
2021-01-29 15:24:57 +0800

    首页资料的渲染完成

--- assets/js/commentAPI.js
+++ assets/js/commentAPI.js
@@ -1,10 +1,16 @@
-axios.defaults.baseURL = 'http://ajax.frontend.itheima.net';
+axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net';
 
 
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
-    console.log('请求发送前');
-    // 在发送请求之前做些什么
+    //获取登录时保存在本地的个人身份信息
+    let token = localStorage.getItem('token');
+    console.log(config);
+    // 在发送请求之前,判断url中是否以/my 开头
+    if (config.url.startsWith('/my')) {
+        //是则在发送的请求中添加请求头和个人身份信息
+        config.headers.Authorization = token
+    }
     return config;
 }, function(error) {
     // 对请求错误做些什么
@@ -14,7 +20,14 @@ axios.interceptors.request.use(function(config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
     console.log('响应前');
-    // 对响应数据做点什么
+    let { message, status } = response.data
+        // 先判断身份验证是否成功
+    if (message == '身份认证失败！' && status == 1) {
+        //清除本地存储的token
+        localStorage.removeItem('token');
+        //跳转到登录页面
+        location.href = './login.html'
+    }
     return response.data;
 }, function(error) {
     // 对响应错误做点什么
---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+$(function() {
+    function getUserInfo() {
+        let { layer } = layui
+        axios({
+            url: '/my/userinfo',
+        }).then(res => {
+            console.log(res);
+            //判断是否获取成功
+            if (res.status != 0) {
+                return layer.msg('获取用户信息失败！')
+            }
+            //渲染页面
+            let { data } = res;
+            //渲染昵称
+            let names = data.nickname || data.username;
+            $('.nickname').text(`欢迎 ${names}`).show();
+            //渲染头像
+            if (data.user_pic) {
+                $('.avart').prop('src', data.user_pic).show();
+                $('.text-avatar').hide();
+            } else {
+                $('.text-avatar').text(names[0].toUpperCase()).show();
+                $('.avart').hide()
+            }
+        })
+
+
+    }
+    getUserInfo();
+    //退出
+    $('.logout').click(function() {
+        //清除本地存储的个人身份验证
+        localStorage.removeItem('token');
+        //跳转到登录页面
+        location.href = './login.html';
+    })
+})
